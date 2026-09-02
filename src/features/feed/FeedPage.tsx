ac659d870482800e08beecff6611bee5e617f894
clcok <ekfrha24@gmail.com>
2026-09-02 12:16:50 +0900

    feat: delete root padding

--- src/features/feed/FeedPage.tsx
+++ src/features/feed/FeedPage.tsx
@@ -65,7 +65,7 @@ export function FeedPage() {
   }
 
   return (
-    <div className="mx-auto min-h-screen max-w-107.5 bg-[#F5FAFE] pb-8">
+    <div className="pb-8">
       <div className="bg-linear-to-b from-indigo-100 via-blue-50 to-white border-b border-slate-100">
         <Header />
         <header className="px-4 pb-4">
