7a0dd8e40d38fe4559fda09d2caca28505526ce9
clcok <ekfrha24@gmail.com>
2026-09-11 17:43:09 +0900

    chore: Delete mock data via connect with API

--- src/mocks/countryMock.ts
+++ src/mocks/countryMock.ts
@@ -1,22 +1,24 @@
-// TODO: 백엔드 국가/도시 enum 정해지면 교체
+import type { Country } from '../types/post'
+
+// TODO: 도시는 아직 백엔드 enum이 없어 프론트 임의 표시용 (실제 전송값은 country만 사용)
 export interface CityOption {
   code: string
   name: string
 }
 
 export interface CountryOption {
-  code: string
+  code: Country // 백엔드 Country enum 값과 동일해야 함 (travelCity로 그대로 전송됨)
   name: string
   flag: string
   cities: CityOption[]
 }
 
 export const COUNTRY_OPTIONS: CountryOption[] = [
-  { code: 'CZ', name: '체코', flag: '🇨🇿', cities: [{ code: 'PRAGUE', name: '프라하' }] },
-  { code: 'FR', name: '프랑스', flag: '🇫🇷', cities: [{ code: 'PARIS', name: '파리' }] },
-  { code: 'AT', name: '오스트리아', flag: '🇦🇹', cities: [{ code: 'VIENNA', name: '빈' }] },
-  { code: 'HU', name: '헝가리', flag: '🇭🇺', cities: [{ code: 'BUDAPEST', name: '부다페스트' }] },
+  { code: 'CZECH_REPUBLIC', name: '체코', flag: '🇨🇿', cities: [{ code: 'PRAGUE', name: '프라하' }] },
+  { code: 'FRANCE', name: '프랑스', flag: '🇫🇷', cities: [{ code: 'PARIS', name: '파리' }] },
+  { code: 'AUSTRIA', name: '오스트리아', flag: '🇦🇹', cities: [{ code: 'VIENNA', name: '빈' }] },
+  { code: 'HUNGARY', name: '헝가리', flag: '🇭🇺', cities: [{ code: 'BUDAPEST', name: '부다페스트' }] },
 ]
 
 // TODO: 실제로는 유저 최근 선택 기록(로컬스토리지 or 서버)에서 가져와야 함
-export const RECENT_COUNTRY_CODES = ['FR', 'AT', 'HU']
+export const RECENT_COUNTRY_CODES: Country[] = ['FRANCE', 'AUSTRIA', 'HUNGARY']
